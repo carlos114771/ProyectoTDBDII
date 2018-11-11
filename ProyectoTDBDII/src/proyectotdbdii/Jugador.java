99e88ccfc5cb3ae9942b22a8d5d7e347404a56f7
carlos114771 <carlosalfredoriveramilian@yahoo.es>
2018-11-11 14:54:51 -0600

    clases

---
+++ ProyectoTDBDII/src/proyectotdbdii/Jugador.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectotdbdii;
+
+/**
+ *
+ * @author Usuario Dell
+ */
+public class Jugador extends Persona{
+
+    public Jugador() {
+    }
+
+    public Jugador(int id, String nombre, String apellido, String fecha_nacimiento) {
+        super(id, nombre, apellido, fecha_nacimiento);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(String fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+    
+}
